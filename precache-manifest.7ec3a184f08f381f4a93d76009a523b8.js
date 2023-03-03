421ed7dde8c3f9bdcbf3ee5143b2e6f7841615c4
arshali2774 <56296638+arshali2774@users.noreply.github.com>
2023-03-03 16:27:10 +0530

    Updates

---
+++ precache-manifest.7ec3a184f08f381f4a93d76009a523b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e8477df928285dc461",
+    "url": "/NameIt/static/css/main.2becb583.chunk.css"
+  },
+  {
+    "revision": "26e8477df928285dc461",
+    "url": "/NameIt/static/js/main.26e8477d.chunk.js"
+  },
+  {
+    "revision": "df84b3d903f5fa9b61e6",
+    "url": "/NameIt/static/js/1.df84b3d9.chunk.js"
+  },
+  {
+    "revision": "dfda4fde926452c6f819",
+    "url": "/NameIt/static/js/runtime~main.dfda4fde.js"
+  },
+  {
+    "revision": "22066736b939b03756b74c5cc843322e",
+    "url": "/NameIt/index.html"
+  }
+];
